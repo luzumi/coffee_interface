f65222ec1f3903bed08e2a843a2a05dbad207a3c
luzumi <corataca@gmail.com>
2023-04-12 15:10:58 +0200

    bugfixing

--- resources/views/menu_help.blade.php
+++ resources/views/menu_help.blade.php
@@ -1,6 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Menü</h1>
-    <p>Hier wird die Menüführung erklärt...</p>
+    <div class="overlay"></div>
+
+    <div class="title_app">
+        <h1>Get me Coffee - </h1>
+
+        <h1 class="title_welcome"> &nbsp;Help - Menu</h1>
+    </div>
+    <ol class="help-text">
+        <li><strong>Getränk auswählen:</strong> Wählen Sie das gewünschte Getränk aus der Getränkeliste aus.</li>
+        <li><strong>Menü-Übersicht:</strong>
+            <ul>
+                <li><em>Home:</em> Zeigt die Startseite mit Getränkeauswahl.</li>
+                <li><em>Getränke:</em> Zeigt eine Liste der verfügbaren Getränke zur Auswahl.</li>
+                <li><em>Guthaben:</em> Zeigt Ihr aktuelles Guthaben an.</li>
+                <li><em>Profil:</em> Zeigt Ihr Profil mit persönlichen Informationen.</li>
+                <li><em>Logout:</em> Loggt Sie aus dem System aus.</li>
+            </ul>
+        </li>
+    </ol>
+
 @endsection
--- resources/views/rfid.blade.php
+++ resources/views/rfid.blade.php
@@ -1,8 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>RFID-Karte</h1>
-    <ol>
+    <div class="overlay"></div>
+
+    <div class="title_app">
+        <h1>Get me Coffee - </h1>
+
+        <h1 class="title_welcome"> &nbsp;Help - RFID</h1>
+    </div>
+    <ol class="help-text">
         <li><strong>Einloggen:</strong> Halten Sie Ihre RFID-Karte an das Lesegerät, um sich einzuloggen.</li>
         <li><strong>Guthaben aufladen:</strong> Sie können Ihr Guthaben bei Alexander Seedorf aufladen.</li>
         <li><strong>Neue RFID-Karte registrieren:</strong>
